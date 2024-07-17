e04d641b1386015d3a5d916c787b8731d1671b34
Samuel Ademujimi <samshotmedia01@gmail.com>
2024-07-17 04:17:17 +0100

    refactor: Update project images URLs to use external hosting

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -11,7 +11,7 @@ const projects = ref([
         stacks: "Vue.js, Javascript, Tailwind CSS, HTML",
         type: "Web App",
         description: "This is a website to discover places and plan your perfect vacation. Explore destinations, manage itineraries, and get personalized recommendations. Built with Vue.js for a seamless user experience.",
-        image: "/src/assets/images/vacation.png",
+        image: "https://i.imghippo.com/files/HqJyh1721185931.png",
     },
     {
         title: "CopyNSync Waitlist",
@@ -21,7 +21,7 @@ const projects = ref([
         stacks: "Vue.js, Javascript, Tailwind CSS, HTML",
         type: "Web App",
         description: "This is a waitlist website for copynsync where users can enter their contact details so they're notified when  the product is live.",
-        image: "/src/assets/images/waitlist.png",
+        image: "https://i.imghippo.com/files/4gY9f1721185958.png",
     },
     {
         title: "CopyNSync Waitlist",
@@ -31,7 +31,7 @@ const projects = ref([
         stacks: "Vue.js, Javascript, Tailwind CSS, HTML",
         type: "Web App",
         description: "This is a project I developed to revolutionize the way we copy and sync text. With CopyNSync, you can effortlessly transfer and synchronize text across multiple devices, enhancing productivity and simplifying workflows. Experience seamless text copying and syncing like never before!",
-        image: "/src/assets/images/waitlist.png",
+        image: "https://i.imghippo.com/files/4gY9f1721185958.png",
     },
 ]);
 </script>
